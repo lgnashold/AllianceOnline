b23efaac0014dd3134ee8f533d49ce9cb944d4f7
Langston Nashold <lgnashold@gmail.com>
2018-11-10 11:40:39 -0600

    Basic Server

--- boardgame/__init__.py
+++ boardgame/__init__.py
@@ -1,4 +1,24 @@
+import os
+
 from flask import Flask
 
 def create_app():
-    app = flask(__name__)
+    # Creates an app
+    # Instance relative config means paths in config files are relative to instance folder
+    app = flask(__name__, instance_relative_config=True)
+    
+    # Edits configurations
+    app.config["SECRET_KEY"] = "123kxjq0kx2rehxj"
+    # instance_path is the instance folder
+    app.config["DATABASE"] = os.path.join(app.instance_path, "boardgame.sqlite")
+    
+    # Insure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Test
+    @app.route('/hello')
+    def hello():
+        return "Hello World"
