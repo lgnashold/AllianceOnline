18d60791285f4579881b80e257a55a9b9fcee642
Langston Nashold <lgnashold@gmail.com>
2018-11-11 00:04:36 -0600

    Lobby

---
+++ boardgame/lobby.py
@@ -0,0 +1,62 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from . import socketio
+from boardgame.db import get_db
+
+from boardgame.emissions import *
+
+from boardgame.colors import colors
+
+from boardgame.db import get_db
+bp = Blueprint('lobby', __name__)
+
+from boardgame.player import *
+
+from boardgame.turn import get_turn, set_turn, increment_turn
+
+
+@socketio.on('connect')
+def connect():
+    print("connected")
+    join_code = session["join_code"]
+    emit_board(join_code)
+    emit_message("%s joined the game!" % session["nickname"], join_code)
+
+
+@bp.route("/startgame")
+def start_game():
+    print("starting!")
+    join_code = session["join_code"]
+    db = get_db()
+    players = get_players(join_code)
+    board = get_board(join_code)
+
+    if get_turn(join_code) != None:
+        return
+    # Inserts starting position
+    if(players["player1"] != None):
+       set_square(join_code, 8, 8, players["player1"])
+    if(players["player2"] != None):
+       set_square(join_code, 12, 12, players["player2"])
+    if(players["player3"] != None):
+       set_square(join_code, 8, 12, players["player3"])
+    if(players["player4"] != None):
+       set_square(join_code, 12, 8, players["player4"])
+
+    set_turn(join_code, 1)
+
+    emit_message("Game started! %s's turn" % players["player1"]["nickname"], join_code)
+    emit_money(join_code,get_players(join_code))
+    emit_board(join_code)
+    return app.route("game.play_game")
+@bp.route("/lobby")
+def enter_lobby():
+    return render_template("lobby.html") 
+    
+@socketio.on('disconnect')
+def disconnect():
+    emit_message("%s left the game..." % session["nickname"], session["join_code"])
+    remove_player(session["join_code"],session["player_num"])
+
