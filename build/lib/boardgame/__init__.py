7fba6ed6976c3658b929b44f6850b1ee4387c5e9
Anna AD <anna.arpacidusseau@gmail.com>
2019-01-12 17:49:13 -0600

    added many files

---
+++ build/lib/boardgame/__init__.py
@@ -0,0 +1,51 @@
+import os
+
+from flask import Flask
+from flask_socketio import SocketIO
+
+# socketio is what you actually use
+socketio = SocketIO()
+
+def create_app():
+    # Creates an app
+    # Instance relative config means paths in config files are relative to instance folder
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Edits configurations
+    app.config["SECRET_KEY"] = "123kxjq0kx2rehxj"
+    # instance_path is the instance folder
+    app.config["DATABASE"] = os.path.join(app.instance_path, "boardgame.sqlite")
+    # Insure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Test
+    @app.route('/hello')
+    def hello():
+        return "Hello World"
+
+    from . import board
+    @app.route('/board')
+    def boardinit():
+         board.create_board("0")
+         return "made board"
+
+    # Initialises database, including registering init-db command
+    from . import db
+    db.init_app(app)
+
+    from . import lobby
+    app.register_blueprint(lobby.bp)
+    
+    from . import game
+    app.register_blueprint(game.bp)
+
+    from . import matchmaking
+    app.register_blueprint(matchmaking.bp)
+
+    socketio.init_app(app)
+
+    # returns created app
+    return app
---
+++ build/lib/boardgame/static/board.js
@@ -0,0 +1,163 @@
+
+var boardPos =[];
+
+var isDragging = false;
+var shouldPlace = true;
+var initMosPos;
+
+var clickedGridSquare = {i:-1,j:-1,cost:-1,color:"white"};
+
+var board_color = "lightgray";
+var ctx;
+
+var c = document.getElementById("gameCanvas");
+var ctx = c.getContext("2d");
+
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight - 230;
+
+var WIDTH = ctx.canvas.width;
+var HEIGHT = ctx.canvas.height;
+var GRID_SIZE = 50;
+
+$(document).ready(function(){
+  $(window).resize(function(){
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight - 230;
+    WIDTH = ctx.canvas.width;
+    HEIGHT = ctx.canvas.height;
+    draw_board();
+  });
+});
+
+var centerX = 0;
+var centerY = 0;
+
+draw_board();
+
+
+
+$("#gameCanvas").mousedown(function(e) {
+  isDragging = true;
+  shouldPlace = true;
+  initMosPos = getMousePos(c,e);
+  //console.log(initMosPos)
+});
+
+$("#gameCanvas").mouseup(function(e) {
+  isDragging = false;
+  gridSquare = get_col(getMousePos(c,e));
+  if(gridSquare.i != -1 && shouldPlace) {
+    console.log(clickedGridSquare.i + " " + gridSquare.i);
+    console.log(clickedGridSquare.j + " " + gridSquare.j);
+
+    if(gridSquare.i === clickedGridSquare.i && gridSquare.j === clickedGridSquare.j) {
+      console.log("should move");
+      makeMove(gridSquare.i,gridSquare.j);
+      clickedGridSquare = {i:-1,j:-1,cost:-1};
+    } else {
+      clickedGridSquare = gridSquare;
+      getCost(gridSquare.i,gridSquare.j);
+    }
+    //console.log(gridSquare.i + " " + gridSquare.j)
+  }
+});
+
+$("#gameCanvas").mousemove(function(e) {
+  //console.log(isDragging);
+  if(isDragging) {
+    shouldPlace = false;
+    finalMosPos = getMousePos(c,e);
+    //console.log(finalMosPos.x - initMosPos.x);
+    centerX += (finalMosPos.x - initMosPos.x);
+    centerY += (finalMosPos.y - initMosPos.y);
+    initMosPos = finalMosPos;
+
+    draw_board();
+  }
+});
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+function get_col(mousePos) {
+  for(var i = 0; i < boardPos.length; i++) {
+    for(var j = 0; j < boardPos[i].length; j++) {
+      if(mousePos.x > (j*GRID_SIZE + centerX) && mousePos.x < (j*GRID_SIZE + centerX) + GRID_SIZE
+        && mousePos.y > i*GRID_SIZE+centerY && mousePos.y < i*GRID_SIZE+centerY + GRID_SIZE) {
+          return {i:i,j:j};
+        }
+    }
+  }
+  return {i:-1,j:-1};
+}
+
+function draw_board() {
+  ctx.fillStyle = board_color;
+  ctx.fillRect(0,0,WIDTH,HEIGHT);
+  for(var i = 0; i < boardPos.length; i++) {
+    for(var j = 0; j < boardPos[i].length; j++) {
+
+      ctx.fillStyle = boardPos[i][j].color;
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 5;
+      ctx.fillRect(j*GRID_SIZE + centerX,i*GRID_SIZE+centerY,GRID_SIZE,GRID_SIZE);
+      ctx.strokeRect(j*GRID_SIZE + centerX,i*GRID_SIZE+centerY,GRID_SIZE,GRID_SIZE);
+      if(boardPos[i][j].name != null) {
+        console.log(boardPos[i][j].name);
+        ctx.fillStyle = "black";
+        ctx.fillText(boardPos[i][j].name,j*GRID_SIZE+centerX + GRID_SIZE/5,i*GRID_SIZE+centerY + GRID_SIZE/4);
+      }
+    }
+  }
+  draw_clicked_square()
+}
+
+function draw_clicked_square() {
+  if(clickedGridSquare.i != -1) {
+    ctx.strokeStyle = clickedGridSquare.color;
+    ctx.lineWidth = 5;
+    ctx.strokeRect(clickedGridSquare.j*GRID_SIZE + centerX,clickedGridSquare.i*GRID_SIZE+centerY,GRID_SIZE,GRID_SIZE);
+    ctx.fillStyle = clickedGridSquare.color;
+    ctx.fillText(clickedGridSquare.cost,clickedGridSquare.j*GRID_SIZE+centerX + GRID_SIZE/2,clickedGridSquare.i*GRID_SIZE+centerY + GRID_SIZE/2);
+  }
+}
+
+function set_board_color(color) {
+  board_color = (LightenDarkenColor(color,60));
+  $("#board").css("background-color",board_color);
+  draw_board();
+}
+
+/** CREDIT: https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors **/
+function LightenDarkenColor(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
