606a031c4e9a93dc3f1b0ed66373f0170906cf51
Anna AD <anna.arpacidusseau@gmail.com>
2019-01-12 20:03:34 -0600

    psycopg updated

--- boardgame/db.py
+++ boardgame/db.py
@@ -17,7 +17,7 @@ def get_db():
         # Location of database is stored in app's config file
         conn = psycopg2.connect(url, sslmode='require')
         conn.set_session(autocommit=True)
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         g.db = cursor
     return g.db
 
